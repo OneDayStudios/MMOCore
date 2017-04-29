9dd5ba3b1e1a10578db1eb3039f3155b22a8b8d1
Bysokar <drakster@me.com>
2017-04-30 01:51:43 +1000

    Adds a comment

--- src/main/java/com/mmocore/module/Gui/GuiElement.java
+++ src/main/java/com/mmocore/module/Gui/GuiElement.java
@@ -90,6 +90,7 @@ public class GuiElement extends Gui {
         if (centred) this.drawCenteredString(mc.fontRenderer, text, posX, posY, color);
         if (!centred) this.drawString(text, posX, posY, color);
         this.stopScaling();
+        // Pop the matrix because that should be whats triggering issues
         GL11.glPopMatrix();
     }
     
