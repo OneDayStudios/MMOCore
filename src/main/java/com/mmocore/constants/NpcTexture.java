66e52c38599c6af04d60c3ffdc4fb7381e166a13
Bysokar <drakster@me.com>
2017-05-03 23:56:59 +1000

    Textures

--- src/main/java/com/mmocore/constants/NpcTexture.java
+++ src/main/java/com/mmocore/constants/NpcTexture.java
@@ -12,6 +12,7 @@ package com.mmocore.constants;
 
 public enum NpcTexture{
     SGC_SOLDIER("customnpcs:textures/entity/stargate/Tauri/tauri_sg_team_member.png", NpcTextureType.Resource),
+    TAURI_EVERETT_YOUNG("customnpcs:textures/entity/stargate/Tauri/tauri_everett_young.png", NpcTextureType.Resource),
     WRAITH_SOLDIER("customnpcs:textures/entity/stargate/Wraith/wraith_soldier.png", NpcTextureType.Resource);
 
     private String value;
