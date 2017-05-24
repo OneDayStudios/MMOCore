784737dde216b20bab884110714182841787ab8e
Bysokar <drakster@me.com>
2017-05-24 19:35:06 +1000

    This is the transition handler v1.0

--- src/main/java/com/mmocore/module/Listener/Listeners/TransitionListener.java
+++ src/main/java/com/mmocore/module/Listener/Listeners/TransitionListener.java
@@ -5,25 +5,68 @@
  */
 package com.mmocore.module.Listener.Listeners;
 
-import com.mmocore.MMOCore;
 import com.mmocore.api.ForgeAPI;
 import com.mmocore.api.GuiAPI;
-import com.mmocore.api.NpcAPI;
-import com.mmocore.api.UniverseAPI;
 import com.mmocore.module.Listener.RegisterableListener;
 import com.mmocore.module.Player.RegisterablePlayer;
 import com.mmocore.constants.GuiSlot;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerChangedDimensionEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
-import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
-import cpw.mods.fml.common.gameevent.TickEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
+import net.minecraft.entity.*;
+import net.minecraft.util.*;
+import net.minecraft.block.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.player.*;
+import net.minecraft.inventory.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.network.*;
+import net.minecraft.network.play.server.*;
+import net.minecraft.potion.*;
+import net.minecraft.server.*;
+import net.minecraft.server.management.*;
+import net.minecraft.tileentity.*;
+import net.minecraft.util.*;
+import net.minecraft.world.*;
+import net.minecraft.world.chunk.*;
+import org.apache.logging.log4j.*;
+import io.netty.channel.*;
+import net.minecraftforge.common.*;
+import net.minecraftforge.common.util.*;
+import net.minecraftforge.common.network.*;
+import cpw.mods.fml.common.network.*;
+import com.mmocore.MMOCore;
+import com.mmocore.api.UniverseAPI;
+import com.mmocore.constants.IntegratedMod;
+import org.apache.logging.log4j.*;
+import io.netty.channel.*;
 
+import net.minecraft.block.*;
+import net.minecraft.entity.*;
+import net.minecraft.entity.player.*;
+import net.minecraft.inventory.*;
+import net.minecraft.item.*;
+import net.minecraft.nbt.*;
+import net.minecraft.network.*;
+import net.minecraft.network.play.server.*;
+import net.minecraft.potion.*;
+import net.minecraft.server.*;
+import net.minecraft.server.management.*;
+import net.minecraft.tileentity.*;
+import net.minecraft.util.*;
+import net.minecraft.world.*;
+import net.minecraft.world.chunk.*;
+
+import net.minecraftforge.common.*;
+import net.minecraftforge.common.util.*;
+import net.minecraftforge.common.network.*;
+import cpw.mods.fml.common.network.*;
+import gcewing.sg.SGBaseTE;
+import gcewing.sg.Trans3;
+import gcewing.sg.Vector3;
 /**
  *
  * @author draks
@@ -32,8 +75,21 @@ public class TransitionListener extends RegisterableListener {
     
     @SubscribeEvent
     public void onLivingUpdate(LivingUpdateEvent e) {
-        //EntityPlayer player = (EntityPlayer)e.player;
-        //TODO: Make a player teleport, and any passengers/vehicles he is with, teleport, when they are below Y=5 in space or above Y=255 on planet.
+        // If the entity is not a player, do nothing.
+        if (!(e.entityLiving instanceof EntityPlayer)) return;
+        EntityPlayer mcPlayer = (EntityPlayer)e.entityLiving;
+        RegisterablePlayer player = MMOCore.getPlayerRegistry().getRegistered(mcPlayer.getUniqueID());
+        if (player.getPosition().isInHyperSpace() || player.getPosition().getCelestialBody() == null || player.getPosition().getCelestialBody().isFake()) return;
+        if (player.getPosition().isInSpace() && player.getPosition().getDPosY() < 0) {
+            Trans3 t = new Trans3(mcPlayer.posX,mcPlayer.posY,mcPlayer.posZ);
+            Trans3 dt = new Trans3(player.getPosition().getCelestialBody().getSpawnX(), 255, player.getPosition().getCelestialBody().getSpawnZ());
+            SGBaseTE.teleportEntityAndRider(mcPlayer, t, dt, player.getPosition().getCelestialBody().getId(), false);
+        }
+        if (!player.getPosition().isInSpace() && player.getPosition().getDPosY() > 300) {
+            Trans3 t = new Trans3(mcPlayer.posX,mcPlayer.posY,mcPlayer.posZ);
+            Trans3 dt = new Trans3(player.getPosition().getCelestialBody().getPosition().getDPosX(), 10, player.getPosition().getCelestialBody().getPosition().getDPosZ());
+            SGBaseTE.teleportEntityAndRider(mcPlayer, t, dt, player.getPosition().getSystem().getId(), false);
+        }
     }
     
     @SubscribeEvent
