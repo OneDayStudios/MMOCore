b1db6bf39d60cc70b2dd61564daae175d6591d8a
Bysokar <drakster@me.com>
2017-06-03 14:32:15 +1000

    Fixes

--- src/main/java/com/mmocore/module/GameEvent/GameEventRegistry.java
+++ src/main/java/com/mmocore/module/GameEvent/GameEventRegistry.java
@@ -8,12 +8,14 @@ package com.mmocore.module.GameEvent;
 import com.mmocore.MMOCore;
 import com.mmocore.module.Galaxy.*;
 import com.mmocore.api.ForgeAPI;
+import com.mmocore.api.UniverseAPI;
 import com.mmocore.constants.NpcModifier;
 import com.mmocore.constants.NpcSpawnMethod;
 import com.mmocore.constants.NpcTexture;
 import com.mmocore.module.AbstractRegistry;
 import com.mmocore.module.Dimension.RegisterableDimension;
 import com.mmocore.module.GameEvent.events.RandomSpawnEvent;
+import com.mmocore.module.GameEvent.events.options.RandomSpawnEventOptions;
 import com.mmocore.module.Npc.RegisterableNpc;
 import com.mmocore.module.NpcFaction.RegisterableNpcFaction;
 import java.util.ArrayList;
@@ -40,6 +42,9 @@ public class GameEventRegistry extends AbstractRegistry<GameEventRegistry, Strin
             RegisterableNpc npc = new RegisterableNpc("Everett Young", "Commander of the Destiny", NpcTexture.TAURI_EVERETT_YOUNG, NpcModifier.RANGED_COMMANDER, NpcSpawnMethod.Static, faction);
             npcs.add(npc);
             RandomSpawnEvent gameEvent = new RandomSpawnEvent("Test Random Spawn", npcs);
+            RandomSpawnEventOptions options = gameEvent.getOptions();
+            options.addSpawnDimension(UniverseAPI.getDimension("P2X-3YZ"));
+            gameEvent.setOptions(options);
             this.register(gameEvent);
         }
         // Not required.
