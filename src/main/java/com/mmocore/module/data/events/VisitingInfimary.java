0f668fe629a2113b545ea8a7b2784daac99f2cf8
Bysokar <drakster@me.com>
2017-06-05 18:33:10 +1000

    Fixes

--- src/main/java/com/mmocore/module/data/events/VisitingInfimary.java
+++ src/main/java/com/mmocore/module/data/events/VisitingInfimary.java
@@ -25,9 +25,9 @@ public class VisitingInfimary extends QuestLocationEvent {
         3,
         8
         );
-        ServerGui enterGui = new ServerGui("SGC Infirmary", "Level X","Stargate Command", (long)2000, GuiSlot.Toast);
+        ServerGui enterGui = new ServerGui("SGC Infirmary", "Level X","Stargate Command", (long)5000, GuiSlot.Toast);
         this.setEnterGui(enterGui);
-        ServerGui completeGui = new ServerGui("Infirmary Located", "Level X","Quest Objective Achieved", (long)2000, GuiSlot.Toast);
+        ServerGui completeGui = new ServerGui("Infirmary Located", "Level X","Quest Objective Achieved", (long)5000, GuiSlot.Toast);
         this.setCompletedGui(completeGui);
     }
     
