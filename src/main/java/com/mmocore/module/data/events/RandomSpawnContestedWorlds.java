a86c28ca688f1adcd1c87cc73ab593fdcb404b0f
Bysokar <drakster@me.com>
2017-06-07 19:19:03 +1000

    undid random spawn wandering for npcs

--- src/main/java/com/mmocore/module/data/events/RandomSpawnContestedWorlds.java
+++ src/main/java/com/mmocore/module/data/events/RandomSpawnContestedWorlds.java
@@ -7,13 +7,11 @@ package com.mmocore.module.data.events;
 
 import com.mmocore.MMOCore;
 import com.mmocore.api.UniverseAPI;
-import com.mmocore.constants.AbstractScale;
 import com.mmocore.module.Dimension.RegisterableDimension;
 import com.mmocore.module.GameEvent.GameEvent;
 import com.mmocore.module.GameEvent.events.RandomSpawnEvent;
 import com.mmocore.module.GameEvent.events.options.SpawnEventOptions;
 import com.mmocore.module.Npc.RegisterableNpc;
-import com.mmocore.module.Npc.options.NpcMovementOptions;
 import com.mmocore.module.data.factions.*;
 import com.mmocore.module.NpcFaction.RegisterableNpcFaction;
 import com.mmocore.module.data.AbstractDictionary;
@@ -29,16 +27,8 @@ public class RandomSpawnContestedWorlds extends RandomSpawnEvent {
 
     public RandomSpawnContestedWorlds() {
             super("Random Spawn - Uncontrolled Worlds");
-            RegisterableNpc sgcSoldier = AbstractDictionary.getNpcByName("SGC Soldier", "Stargate Command");
-            NpcMovementOptions mOpts = sgcSoldier.getMovementOptions();
-            mOpts.setMovementTypeWandering(AbstractScale.Medium);
-            sgcSoldier.setMovementOptions(mOpts);
-            RegisterableNpc jaffaSoldier = AbstractDictionary.getNpcByName("Jaffa Soldier", "Goauld Loyal Jaffa");
-            NpcMovementOptions jMOpts = jaffaSoldier.getMovementOptions();
-            jMOpts.setMovementTypeWandering(AbstractScale.Medium);
-            jaffaSoldier.setMovementOptions(jMOpts);
-            this.addNpc(sgcSoldier);
-            this.addNpc(jaffaSoldier);
+            this.addNpc(AbstractDictionary.getNpcByName("SGC Soldier", "Stargate Command"));
+            this.addNpc(AbstractDictionary.getNpcByName("Jaffa Soldier", "Goauld Loyal Jaffa"));
             SpawnEventOptions options = this.getOptions();
             for (RegisterableDimension dimension : UniverseAPI.getDimensions()) {
                 if (dimension.getFaction() == null) options.addSpawnDimension(dimension);
